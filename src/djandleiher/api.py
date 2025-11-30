cad5ae154178f2783f2ad3db3223747ce15ee318
Eike Fokken <e.fokken+github@posteo.de>
2025-11-30 22:27:16 +0100

    first version with jwt in place.

--- src/djandleiher/api.py
+++ src/djandleiher/api.py
@@ -1,9 +1,12 @@
-from django.http import HttpRequest, HttpResponse
-from ninja import NinjaAPI
+from django.http import HttpRequest
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
 
-api = NinjaAPI()
+# api = NinjaAPI()
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
 
 
 @api.get("/hello")
-def hello(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello world")
+def hello(request: HttpRequest) -> str:
+    return "Hello world"
--- src/djandleiher/settings.py
+++ src/djandleiher/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "ninja",
+    "ninja_extra",
+    "ninja_jwt",
 ]
 
 MIDDLEWARE = [
