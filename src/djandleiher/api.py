12950c55c5df3abf72197a7d0ef8f3bd487bb72a
Eike Fokken <e.fokken+github@posteo.de>
2025-11-30 19:44:28 +0100

    hello world is running

--- src/djandleiher/api.py
+++ src/djandleiher/api.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest, HttpResponse
 from ninja import NinjaAPI
 
 api = NinjaAPI()
+
+
+@api.get("/hello")
+def hello(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello world")
